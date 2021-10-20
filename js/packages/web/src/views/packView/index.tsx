eef11cc19e2f55389897d445b8b268f3180431a6
Serhii <serhiikoziy@gmail.com>
2021-10-20 16:09:39 +0300

    feat: pack-view-add-list

--- js/packages/web/src/views/packView/index.tsx
+++ js/packages/web/src/views/packView/index.tsx
@@ -1,16 +1,38 @@
 import React  from 'react';
-import { Typography } from 'antd';
+import { Row, Col, Typography } from 'antd';
 
 const { Text } = Typography;
 
-const PackView = () => {
+export const PackView = () => {
+  const mockBlocks = Array.from({length: 10}, (v, i) => i);
+
   return (
     <div className="pack-view">
+      <Row>
+        <Col md={16}>
+          <div className="pack-view__list">
+            {
+              mockBlocks.map((block, i) => (
+                <div key={i} className="pack-view__block">
+                  <div className="pack-view__square">
+                    <span>{ i === 0 ? '?' : i + 1}</span>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </Col>
+        <Col md={8} >
+          <div className="pack-view__sidebar">
+            <h4>Morning</h4>
+            <div>
 
+            </div>
+          </div>
+        </Col>
+      </Row>
 
     </div>
   );
 };
-
-export default PackView;
 
