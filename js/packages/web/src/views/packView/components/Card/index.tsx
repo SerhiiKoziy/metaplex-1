a9d3b5cc95aff2a6e231edae82bd47d0083605a6
Serhii <serhiikoziy@gmail.com>
2021-10-25 13:06:07 +0300

    feat: redeem-modal

--- js/packages/web/src/views/packView/components/Card/index.tsx
+++ js/packages/web/src/views/packView/components/Card/index.tsx
@@ -1,7 +1,11 @@
 import React  from 'react';
 
-const Card = ({ value }) => (
-  <div key={value} className="pack-view__block">
+const Card = ({ value, onOpen }) => (
+  <div
+    key={value}
+    className="pack-view__block"
+    onClick={() => onOpen(true)}
+  >
     <div className="pack-view__square">
       <span>{ value === 0 ? '?' : value + 1}</span>
     </div>
